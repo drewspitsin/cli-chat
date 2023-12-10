5d311c21d759a08ef3a41b92e230ac566bcd6602
Андрей Спицин <34519147+drewspitsin@users.noreply.github.com>
2023-12-10 23:31:06 +0300

    Add files via upload

--- cmd/root/root.go
+++ cmd/root/root.go
@@ -1,16 +1,76 @@
 package root
 
 import (
+	"context"
+	"fmt"
 	"log"
 	"os"
+	"sync"
+	"time"
 
+	desc "github.com/drewspitsin/cli-chat/pkg/chat_api_v1"
+	"github.com/fatih/color"
 	"github.com/spf13/cobra"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+const (
+	address = "localhost:50052"
 )
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "chat-app",
 	Short: "cli-chat",
+}
+
+var testCmd = &cobra.Command{
+	Use:   "spam",
+	Short: "TestSpam",
+	Run: func(cmd *cobra.Command, args []string) {
+		conn, err := grpc.Dial(address, grpc.WithTransportCredentials(insecure.NewCredentials()))
+		if err != nil {
+			log.Fatalf("failed to connect to server: %v", err)
+		}
+		defer conn.Close()
+
+		ctx := context.Background()
+		client := desc.NewChatV1Client(conn)
+
+		// Создаем новый чат на сервере
+		chatID, err := createChat(ctx, client)
+		if err != nil {
+			log.Fatalf("failed to create chat: %v", err)
+		}
+
+		log.Printf(fmt.Sprintf("%s: %s\n", color.GreenString("Chat created"), color.YellowString(chatID)))
+
+		wg := sync.WaitGroup{}
+		wg.Add(2)
+
+		// Подключаемся к чату от имени пользователя oleg
+		go func() {
+			defer wg.Done()
+
+			err = connectChat(ctx, client, chatID, "oleg", 5*time.Second)
+			if err != nil {
+				log.Fatalf("failed to connect chat: %v", err)
+			}
+		}()
+
+		// Подключаемся к чату от имени пользователя ivan
+		go func() {
+			defer wg.Done()
+
+			err = connectChat(ctx, client, chatID, "ivan", 7*time.Second)
+			if err != nil {
+				log.Fatalf("failed to connect chat: %v", err)
+			}
+		}()
+
+		wg.Wait()
+	},
 }
 
 var createCmd = &cobra.Command{
@@ -61,6 +121,7 @@ func Execute() {
 func init() {
 	rootCmd.AddCommand(createCmd)
 	rootCmd.AddCommand(deleteCmd)
+	rootCmd.AddCommand(testCmd)
 
 	createCmd.AddCommand(createUserCmd)
 	deleteCmd.AddCommand(deleteUserCmd)
